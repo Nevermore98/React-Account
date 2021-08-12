256cf4a48236b4391e4ec425812b483c94e2f49f
Nevermore98 <Nevermorey98@qq.com>
2021-08-12 22:09:57 +0800

    引入 icon 图标

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,36 +2,59 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
+
+
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
 
     > li {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
       width: 33.3333%;
-      text-align: center;
-      padding: 16px;
+      padding: 4px;
+
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
 
 const Nav = () => {
-  return(
+  return (
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref={'#tag'}/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref={'#money'}/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref={'#chart'}/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
 export default Nav;
