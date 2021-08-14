53c7111cc3db620a89a096cf285601fb17d536d2
Nevermore98 <Nevermorey98@qq.com>
2021-08-14 17:34:49 +0800

    完成 TagSection 的标签选择与新增功能

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-grow: 1;
   flex-direction: column;
@@ -20,6 +21,9 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected {
+        background: #888;
+      }
     }
   }
 
@@ -32,4 +36,44 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const onAddTag = () => {
+    const tagName = window.prompt('请输入新标签名');
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const getClass = (tag:string) => selectedTags.indexOf(tag)>=0?'selected':''
+
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag}
+              onClick={() => {onToggleTag(tag);}}
+              className={getClass(tag)}>
+            {tag}
+          </li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
+
 export default TagsSection;
