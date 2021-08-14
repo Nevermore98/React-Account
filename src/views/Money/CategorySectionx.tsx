45e6994dde7bd82c2f37f0af750e2f52a59c7598
Nevermore98 <Nevermorey98@qq.com>
2021-08-14 18:38:43 +0800

    完成 CategorySection 收入支出切换功能

--- src/views/Money/CategorySectionx.tsx
+++ src/views/Money/CategorySectionx.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
+// import set = Reflect.set;
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
 
   > ul {
@@ -26,5 +28,37 @@ const CategorySection = styled.section`
     }
   }
 `;
+const CategorySection: React.FC = () => {
+  // const [category, setCategory] = useState('-')
+  // return(
+  //   <Wrapper>
+  //     <ul>
+  //       <li className={category === '-' ? 'selected' : ''}
+  //           onClick={() => {setCategory('-')}}
+  //       >支出</li>
+  //       <li className={category === '+' ? 'selected' : ''}
+  //           onClick={() => {setCategory('+')}}
+  //       >收入</li>
+  //     </ul>
+  //   </Wrapper>
+  // )
+  const categoryMap = {'-': '支出', '+': '收入'};
+  type Keys = keyof typeof categoryMap
+  const [categoryList] = useState<Keys[]>(['-', '+']);
+  const [category, setCategory] = useState('-');
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li key={c}
+              className={category === c ? 'selected' : ''}
+              onClick={() => setCategory(c)}
+          >{categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper>
+  );
+};
 
 export default CategorySection;
