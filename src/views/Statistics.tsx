1c2b18b8286676ab0da71fb6dd25a7e22a4c51bd
Nevermore98 <Nevermorey98@qq.com>
2021-09-01 16:00:04 +0800

    消除警告

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -38,7 +38,7 @@ function Statistics() {
   const hash: { [K: string]: RecordItem[] } = {};
   const selectedRecords = records.filter(r => r.category === category);
 
-  selectedRecords.map(r => {
+  selectedRecords.forEach(r => {
     const key = day(r.createdAt).format('YYYY年MM月DD日');
     if (!(key in hash)) {
       hash[key] = [];
@@ -59,7 +59,7 @@ function Statistics() {
         <CategorySection value={category}
                          onChange={value => setCategory(value)}/>
       </CategoryWrapper>
-      {array.map(([date, records]) => <div>
+      {array.map(([date, records]) => <div key={date}>
         <Header>
           {date}
         </Header>
