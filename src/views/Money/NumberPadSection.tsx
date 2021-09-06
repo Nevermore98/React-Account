93eff362b2419abd622f41e30fb0ae75df0742b9
Nevermore98 <nevermorey98@qq.com>
2021-09-06 23:36:11 +0800

    实现记账成功后重置 output 为 0

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -27,6 +27,7 @@ const NumberPadSection: React.FC<Props> = (props) => {
     if (text === null) {return;}
     if (text === '完成') {
       if (props.onOk) {props.onOk();}
+      setOutput('0');
       return;
     }
     if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
